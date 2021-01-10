53c1b7c532b4af554780f5b11d0f88b2de9ad3be
Keith <764058263@qq.com>
2021-01-10 23:18:08 +0800

    Lock

--- src/main/java/com/demo/Test.java
+++ src/main/java/com/demo/Test.java
@@ -1,14 +1,31 @@
 package com.demo;
 
-import java.util.Arrays;
+import java.util.concurrent.Exchanger;
+import java.util.concurrent.Phaser;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.concurrent.atomic.LongAdder;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
-/**
- * @author keith
- */
 public class Test {
+
     public static void main(String[] args) {
-        String a = "ab23123123||c123121||2";
-        String[] split = a.split("\\|\\|");
-        System.out.println(split[0]);
+        Exchanger<String> exchanger = new Exchanger<>();
+        new Thread(() -> {
+            try {
+                exchanger.exchange("1");
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+        new Thread(() -> {
+            try {
+                exchanger.exchange("2");
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+
     }
 }
